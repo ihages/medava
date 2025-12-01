7702332fa41bc7ab1fdafd5cc748e9423f8d92fa
ihages <ihageman23@gmail.com>
2025-12-01 11:23:24 -0500

    example push

--- src/main/java/edu/uc/cs3003/medava/Medicine.java
+++ src/main/java/edu/uc/cs3003/medava/Medicine.java
@@ -1,5 +1,25 @@
 package edu.uc.cs3003.medava;
 
 public class Medicine {
+    private String mMedicineName;mMedicineName=medicineName;
 
+    public String getMedicineName() {
+        return mMedicineName;
+    }
+
+    public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
+        if (this.minimumTemperature() <= lowTemperature &&
+                highTemperature <= this.maximumTemperature()) {
+            return true;
+        }
+        return false;
+    }
+
+    public double minimumTemperature() {
+        return 0.0;
+    }
+
+    public double maximumTemperature() {
+        return 100.0;
+    }
 }
--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -1,3 +1,23 @@
 package edu.uc.cs3003.medava;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Transporter {
+
+    private String mTransporterName;
+
+    public String getTransporterName() {
+        return mTransporterName;
+    }
+
+    public List<Medicine> goods;
+
+    {
+        goods = new ArrayList<Medicine>();
+    }
+
+    public void ship() {
+        // Do some shipping!
+    }
 }
